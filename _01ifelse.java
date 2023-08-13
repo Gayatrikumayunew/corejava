25d824c3fac51c2181bfe507bce66f626bba3a19
Gayatrikumayunew <126497909+Gayatrikumayunew@users.noreply.github.com>
2023-08-13 20:37:42 +0530

    Add files via upload

---
+++ _01ifelse.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gayatri
+ */
+public class _01ifelse {
+    public static void main(String[]args){
+//        int age=12;
+//        if(age>18)
+//            System.out.println("you can drive");
+//        else
+//            System.out.println("you cannot drive");
+//        
+//short form of ifelse
+//int age=112;
+//int b=45;
+//int max= (age>b)?age:b;
+//System.out.println(max);
+
+int a=17,b=14,c=12,max;
+max=(a>b)?(a>c)?a:c:(b>c)?b:c;
+System.out.println(max);
+
+        
+    }
+    
+}
